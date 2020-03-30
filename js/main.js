4b9668e1d35c88f869eec1351f201903cbaaa47c
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-03-30 22:24:59 +0900

    add menu-list

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const show = document.getElementById('show');
+const hide = document.getElementById('hide');
+
+show.addEventListener('click',function(){
+    document.body.className = 'menu-open';
+});
+
+hide.addEventListener('click',function(){
+    document.body.className = '';
+});
