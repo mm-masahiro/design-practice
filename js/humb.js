d511ea302402df3de488fe55b1c8d0edc27710ee
mm-masahiro <hand5.mm9.mzh@gmail.com>
2020-04-01 20:22:36 +0900

    make hum_menu

--- js/humb.js
+++ js/humb.js
@@ -1,12 +1,23 @@
 'use strict'
 
-const menu = document.getElementById('menu')
-const hum = document.getElementById('hum')
+// const menu = document.getElementById('menu')
+// const hum = document.getElementById('hum')
 
-menu.addEventListener('click',function(){
+// menu.addEventListener('click',function(){
+//     document.body.className = 'menu-open';
+// });
+
+// hum.addEventListener('click',function(){
+//     document.body.className = '';
+// });
+
+let show = document.getElementById('show');
+let hide = document.getElementById('hide');
+
+show.addEventListener('click',function(){
     document.body.className = 'menu-open';
 });
 
-hum.addEventListener('click',function(){
+hide.addEventListener('click',function(){
     document.body.className = '';
 });
